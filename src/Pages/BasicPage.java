18bc2b06263bb6f67b3026ce789edf6f00251348
Stefan Jocic <jocicstefan91@gmail.com>
2022-02-19 15:00:43 +0100

    BasicPage corection, js added

--- src/Pages/BasicPage.java
+++ src/Pages/BasicPage.java
@@ -1,19 +1,28 @@
 package Pages;
 
+import java.time.Duration;
 import java.util.List;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasicPage {
 
 	protected WebDriver driver;
+	protected JavascriptExecutor js;
+	protected WebDriverWait wait;
 	
-	public BasicPage(WebDriver driver) {
+	public BasicPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		
 		this.driver=driver;
+		this.wait=wait;
+		this.js=js;
 		
 	}
+	
+	
 	
 	public abstract WebElement getLocation();
 	
