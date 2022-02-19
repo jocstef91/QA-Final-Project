395878e77563e58c1bbaa54c297d89c3d2dcf7c3
Stefan Jocic <jocicstefan91@gmail.com>
2022-02-19 16:20:44 +0100

    CartSummaryPage completed

---
+++ src/Pages/CartSummaryPage.java
@@ -0,0 +1,38 @@
+package Pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CartSummaryPage extends BasicPage {
+
+	public CartSummaryPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+		
+	}
+	
+	@Override
+	public WebElement getLocation() {
+		
+		return driver.findElement(By.className("location-selector"));
+		
+	}
+
+	@Override
+	public List<WebElement> getMenuBar() {
+		return driver.findElements(By.className("navs--main"));
+	}
+	
+	private WebElement getClearAllBtn() {
+		return driver.findElement(By.xpath("//a[contains(@onClick,'clearCartItems()')]"));
+	}
+	
+	public void clearAll() {
+		getClearAllBtn().click();
+	}
+
+}
